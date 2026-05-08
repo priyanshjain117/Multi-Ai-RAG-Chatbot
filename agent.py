92d6303beacadc38e0edb5c28cdaabace2619f77
priyanshjain117 <jainpriyanshu117@gmail.com>
2026-05-08 15:07:04 +0530

    agent object for implementation implemented

---
+++ agent.py
@@ -0,0 +1,39 @@
+from vectorstore import create_vectorstore
+
+from router import create_router
+
+from wiki_tool import create_wiki_tool
+
+from graph import build_graph
+
+
+def initialize_agent(
+    groq_api_key,
+    astra_token,
+    astra_db_id,
+    urls,
+):
+
+    vectorstore = create_vectorstore(
+        urls,
+        astra_token,
+        astra_db_id,
+    )
+
+    retriever = vectorstore.as_retriever()
+
+    router = create_router(
+        groq_api_key,
+        urls,
+    )
+
+    wiki_tool = create_wiki_tool()
+
+    app = build_graph(
+        retriever,
+        wiki_tool,
+        router,
+        groq_api_key,
+    )
+
+    return app
